4cb400d138e26ad627ba3bc2125a34937190106d
kenho01 <kenho9674@gmail.com>
2024-06-02 08:03:09 +0800

    fix getAuthorization()

--- main.py
+++ main.py
@@ -20,20 +20,13 @@ import base64
 DATABASE_LOCATION = "sqlite://spotify_tracks.sqlite"
 REDIRECT_URI = "http://localhost:8888/callback"
 AUTH_URL = "https://accounts.spotify.com/authorize"
+REDIRECT_URI = 'http://localhost:8888/callback'
 
 
 def configure():
     load_dotenv()
 
-app = Flask(__name__)
-
-client_id = 'CLIENT_ID'
-client_secret = 'CLIENT_SECRET'
-redirect_uri = 'http://localhost:8888/callback'
-
-
-@app.route('/login')
-def login():
+def getAuthorization():
     configure()
     client_id = os.getenv('CLIENT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
@@ -44,40 +37,37 @@ def login():
         'response_type': 'code',
         'client_id': client_id,
         'scope': scope,
-        'redirect_uri': redirect_uri,
+        'redirect_uri': REDIRECT_URI,
         'state': state
     }
-    url = 'https://accounts.spotify.com/authorize?' + '&'.join([f'{key}={value}' for key, value in query_params.items()])
+    url = AUTH_URL + '?' + urlencode(query_params)
     print(url)
     return redirect(url)
 
-@app.route('/callback')
-def callback():
-    code = request.args.get('code')
-    state = request.args.get('state')
-    print(code)
-    print(state)
-
-    if state is None:
-        return redirect('/#?error=state_mismatch')
-    
+def getToken():
+    configure()
+    client_id = os.getenv('CLIENT_ID')
+    client_secret = os.getenv('CLIENT_SECRET')
     auth_options = {
-        'url': 'https://accounts.spotify.com/api/token',
-        'data': {
-            'code': code,
-            'redirect_uri': redirect_uri,
-            'grant_type': 'authorization_code'
-        },
-        'headers': {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + base64.b64encode(f'{client_id}:{client_secret}'.encode()).decode()
+        'url' : 'https://accounts.spotify.com/api/token',
+        'data' : {
+        'code' : os.getenv('CODE'),
+        'redirect_uri' : REDIRECT_URI,
+        'grant_type' : 'authorization_code'
+    },
+        'headers' : {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'Authorization' : 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode()).decode('utf-8')
         }
     }
-    
     response = requests.post(auth_options['url'], data=auth_options['data'], headers=auth_options['headers'])
-    token_info = response.json()
-    
-    return jsonify(token_info)
+    response_data = response.json()
+    print("Response data" + str(response_data))
+
+def main():
+    getAuthorization()
+    getToken()
 
 if __name__ == '__main__':
-    app.run(port=8888)
+    main()
+    
