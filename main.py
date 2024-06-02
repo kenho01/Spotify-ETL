3c73c2629ca8e567efdef1319353939165a23c43
kenho01 <kenho9674@gmail.com>
2024-06-02 09:39:52 +0800

    extraction logic

--- main.py
+++ main.py
@@ -1,73 +1,95 @@
-import sqlalchemy
-import pandas as pd
-from sqlalchemy.orm import sessionmaker
+import urllib.parse
+from flask import Flask, redirect, request, jsonify, session
+from datetime import datetime
 import requests
-from flask import Flask, request, redirect, jsonify
-from requests.auth import HTTPBasicAuth
-import json
-import string
-import random
-import datetime
 import os
 from dotenv import load_dotenv
-from datetime import timedelta
-from urllib.parse import urlencode
-import sqlite3
-import base64
-import string
-import base64
 
-DATABASE_LOCATION = "sqlite://spotify_tracks.sqlite"
-REDIRECT_URI = "http://localhost:8888/callback"
-AUTH_URL = "https://accounts.spotify.com/authorize"
-REDIRECT_URI = 'http://localhost:8888/callback'
-
+app = Flask(__name__)
+app.secret_key = 'Sa2BKQUhHyJ0lNYcBs7FZgU3VVMj7hVZ'
+REDIRECT_URI = 'http://localhost:5000/callback'
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+API_BASE_URL = 'https://api.spotify.com/v1/'
 
 def configure():
     load_dotenv()
 
-def getAuthorization():
+@app.route('/')
+def index():
+    return "Hi! <a href='/login'> Login with Spotify </a>"
+
+@app.route('/login')
+def login():
     configure()
     client_id = os.getenv('CLIENT_ID')
-    client_secret = os.getenv('CLIENT_SECRET')
-    state = random.randint(0,16)
     scope = 'user-read-recently-played'
-    
-    query_params = {
-        'response_type': 'code',
-        'client_id': client_id,
-        'scope': scope,
-        'redirect_uri': REDIRECT_URI,
-        'state': state
+    params = {
+        'client_id' : client_id,
+        'response_type' : 'code',
+        'scope' : scope,
+        'redirect_uri' : REDIRECT_URI,
+        # For debugging, force user to login everytime
+        'show_dialog' : True
     }
-    url = AUTH_URL + '?' + urlencode(query_params)
-    print(url)
-    return redirect(url)
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+    return redirect(auth_url)
 
-def getToken():
+@app.route('/callback')
+def callback():
     configure()
     client_id = os.getenv('CLIENT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
-    auth_options = {
-        'url' : 'https://accounts.spotify.com/api/token',
-        'data' : {
-        'code' : os.getenv('CODE'),
-        'redirect_uri' : REDIRECT_URI,
-        'grant_type' : 'authorization_code'
-    },
-        'headers' : {
-            'Content-Type' : 'application/x-www-form-urlencoded',
-            'Authorization' : 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode()).decode('utf-8')
+    if 'error' in request.args:
+        return jsonify({"error" : request.args['error']})
+    if 'code' in request.args:
+        req_body = {
+            'code' : request.args['code'],
+            'grant_type' : 'authorization_code',
+            'redirect_uri' : REDIRECT_URI,
+            'client_id' : client_id,
+            'client_secret' : client_secret
         }
-    }
-    response = requests.post(auth_options['url'], data=auth_options['data'], headers=auth_options['headers'])
-    response_data = response.json()
-    print("Response data" + str(response_data))
+    response = requests.post(TOKEN_URL, data=req_body)
+    token_info = response.json()
 
-def main():
-    getAuthorization()
-    getToken()
+    session['access_token'] = token_info['access_token']
+    session['refresh_token'] = token_info['refresh_token'] 
+    session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+
+    return redirect('/recentlyplayed')
+
+@app.route('/recentlyplayed')
+def recentlyplayed():
+    if 'access_token' not in session:
+        return redirect('login')
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh-token')
+    headers = {
+        'Authorization' : f"Bearer {session['access_token']}"
+    }
+
+    response = requests.get(API_BASE_URL + 'me/player/recently-played', headers=headers)
+    recentlyplayed = response.json()
+    return jsonify(recentlyplayed)
+
+@app.route('/refresh-token')
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    if datetime.now().timestamp() > session['expires_at']:
+        req_body = {
+            'grant_type' : 'refresh_token',
+            'refresh_token' : session['refresh_token'],
+            'client_id' : CLIENT_ID,
+            'client_secret' : CLIENT_SECRET
+        }
+    response = requests.post(TOKEN_URL, data=req_body)
+    new_token_info = response.json()
+    session['access_token']  = new_token_info['access_token']
+    session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
+
+    return redirect('/recentlyplayed')
 
 if __name__ == '__main__':
-    main()
-    
+    app.run(host='0.0.0.0', debug=True)
